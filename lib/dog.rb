823957c74d236362d379e4775870c8c0825c9a3d
persia sarram <kittycuppers01@gmail.com>
2018-06-07 23:38:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,7 @@
 # dog.rb
+class Dog
+   def k9(fido, snoopy, lassie)
+    @fido = fido
+    @snoopy = snoopy
+    @lassie = lassie
+  end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,6 @@
 # person.rb
+class People
+   def humans(adele goldberg, alan kay)
+    @adele_goldberg = adele goldberg
+    @alan_kay = alan kay
+  end
